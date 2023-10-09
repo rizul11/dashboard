d572403f29e2130a4b82cf4c5288efb75c863918
rizul11 <rizulsharma.offical@gmail.com>
2023-10-09 15:41:31 +0530

    added some comments for better understanding

--- src/features/postDetailsActions.js
+++ src/features/postDetailsActions.js
@@ -9,13 +9,12 @@ export const fetchPostDetails = createAsyncThunk(
       // Perform an API request to fetch post details based on the postId
       const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${postId}`);
       const postDetails = await response.json();
-
       // Dispatch a success action with the fetched post details
-      dispatch(setPostDetails(postDetails));
-
-      return postDetails; // Return the fetched data if needed
-    } catch (error) {
-      // Handle any errors or dispatch an error action if needed
+     dispatch(setPostDetails(postDetails));
+     return postDetails;
+       // Return the fetched data if needed
+    } 
+    catch (error) { // Handle any errors or dispatch an error action if needed
       console.error("Error fetching post details:", error);
       throw error;
     }
--- src/pages/PostDetail.jsx
+++ src/pages/PostDetail.jsx
@@ -1,33 +1,9 @@
-// import React, { useEffect } from "react";
-// import { useSelector, useDispatch } from "react-redux";
 
-// import { fetchPostDetails } from "../features/postDetailsActions";
-
-// function PostDetails() {
-//   const postId = useSelector((state) => state.general.param);
-//   console.log("post id", postId);
-//   const dispatch = useDispatch();
-//   const postDetails = useSelector((state) => state.postDetails);
-
-//   useEffect(() => {
-//     dispatch(fetchPostDetails(postId));
-//   }, [dispatch, postId]);
-//   console.log("details", postDetails);
-//   return (
-//     <div className="post-detail-container">
-//       <h3>{postDetails?.title}</h3>
-//       <p>{postDetails?.body}</p>
-//       <p>UserId: {postDetails?.userId}</p>
-//     </div>
-//   );
-// }
-
-// export default PostDetails;
 
 import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { fetchPostDetails } from "../features/postDetailsActions";
-import { Link } from "react-router-dom"; // Import Link from react-router-dom
+import { Link } from "react-router-dom";
 
 function PostDetails() {
   const postId = useSelector((state) => state.general.param);
@@ -52,7 +28,9 @@ function PostDetails() {
               <h3 className="card-title">{postDetails?.title}</h3>
               <p className="card-text">{postDetails?.body}</p>
               <p className="card-text">
-                <small className="text-muted">UserId: {postDetails?.userId}</small>
+                <small className="text-muted">
+                  UserId: {postDetails?.userId}
+                </small>
               </p>
             </div>
           </div>
