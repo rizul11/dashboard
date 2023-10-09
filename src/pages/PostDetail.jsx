24ffc680f2e078d7a8b6d4fa60382f523e5adfac
rizul11 <rizulsharma.offical@gmail.com>
2023-10-09 13:26:29 +0530

    added some styling

--- src/pages/PostDetail.jsx
+++ src/pages/PostDetail.jsx
@@ -1,7 +1,33 @@
+// import React, { useEffect } from "react";
+// import { useSelector, useDispatch } from "react-redux";
+
+// import { fetchPostDetails } from "../features/postDetailsActions";
+
+// function PostDetails() {
+//   const postId = useSelector((state) => state.general.param);
+//   console.log("post id", postId);
+//   const dispatch = useDispatch();
+//   const postDetails = useSelector((state) => state.postDetails);
+
+//   useEffect(() => {
+//     dispatch(fetchPostDetails(postId));
+//   }, [dispatch, postId]);
+//   console.log("details", postDetails);
+//   return (
+//     <div className="post-detail-container">
+//       <h3>{postDetails?.title}</h3>
+//       <p>{postDetails?.body}</p>
+//       <p>UserId: {postDetails?.userId}</p>
+//     </div>
+//   );
+// }
+
+// export default PostDetails;
+
 import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
-
 import { fetchPostDetails } from "../features/postDetailsActions";
+import { Link } from "react-router-dom"; // Import Link from react-router-dom
 
 function PostDetails() {
   const postId = useSelector((state) => state.general.param);
@@ -13,11 +39,25 @@ function PostDetails() {
     dispatch(fetchPostDetails(postId));
   }, [dispatch, postId]);
   console.log("details", postDetails);
+
   return (
-    <div className="post-detail-container">
-      <h3>{postDetails?.title}</h3>
-      <p>{postDetails?.body}</p>
-      <p>UserId: {postDetails?.userId}</p>
+    <div className="container mt-4">
+      <div className="row">
+        <div className="col-md-8 offset-md-2">
+          <div className="card">
+            <div className="card-body">
+              <Link to="/posts" className="btn btn-primary mb-3">
+                Back to Posts
+              </Link>
+              <h3 className="card-title">{postDetails?.title}</h3>
+              <p className="card-text">{postDetails?.body}</p>
+              <p className="card-text">
+                <small className="text-muted">UserId: {postDetails?.userId}</small>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
